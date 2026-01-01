a7345685655f062a0c7016324abb2c80d87414ca
riccardo <riccardo.ravaro@datatamer.ai>
2026-01-01 14:03:52 +0700

    chore: Bump version to 0.2.8

--- crawlerWhipAI/version.py
+++ crawlerWhipAI/version.py
@@ -1,6 +1,6 @@
 """Version information for CrawlerWhipAI."""
 
-__version__ = "0.2.7"
+__version__ = "0.2.8"
 __author__ = "DataTamer.ai"
 __author_email__ = "info@datatamer.ai"
 __license__ = "Apache-2.0"
